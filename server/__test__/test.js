f90a176b222f428633d568a2763c1c7229e0f769
Tony3243 <113482331+Tony3243@users.noreply.github.com>
2026-04-23 15:13:32 -0700

    Made bug fixes in my test cases.

--- server/__test__/test.js
+++ server/__test__/test.js
@@ -8,13 +8,14 @@ describe("Jobs API", () => {
         test("Should recieve a status of 200 and be an array", async () => {
             const response = await request(app).get("/api/jobs");
             expect(response.status).toBe(200);
-            expect(Array.isArray(response.body.jobs).toBe(true))
+            expect(Array.isArray(response.body.jobs)).toBe(true)
         })
         test("Check if each object in Array has the right properties", async () => {
             const response = await request(app).get("/api/jobs");
             const arrayOfJobs = response.body.jobs;
-            const properties = arrayOfJobs.forEach((element) => {
-                element.id && element.company_name && element.job_title && element.application_status && element.application_date && element.notes ? true : false;
+            expect(arrayOfJobs.length > 0)
+            const properties = arrayOfJobs.every((element) => {
+                return element.id && element.company_name && element.job_title && element.application_status && element.application_date && element.notes;
             })
             expect(properties).toBe(true)
         })
